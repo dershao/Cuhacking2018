e43c748b5a899e02c411a2ad958ca8e8ddcb5bc0
Derek Shao <shao.derek.now@gmail.com>
2018-03-31 14:42:35 -0400

    Added reddit scraper

---
+++ feelsgood/emotion/Reddit/subreddits.py
@@ -0,0 +1,21 @@
+
+#String constants for subreddits
+
+
+
+#happy
+UPLIFTINGNEWS = "upliftingnews"
+
+#sad
+GETMOTIVATED = "getmotivated"
+WHOLESOMEMEMES = "wholesomemes"
+
+EMOTION_DICTIONARY = {
+	
+	"happy" : UPLIFTINGNEWS,
+	"sad" : GETMOTIVATED,
+	"angry" : WHOLESOMEMEMES,
+	"surprise" : UPLIFTINGNEWS,
+	"neutral" : WHOLESOMEMEMES,
+}
+
